e95145b152f99b137a42240fce9b6fefc8a00db0
unknown <daffaatha2806@gmail.com>
2026-05-16 21:31:40 +0700

    feat(admin): add admin features for report

--- server/routes/report.route.js
+++ server/routes/report.route.js
@@ -2,15 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 const reportController = require("../controllers/report.controller");
+const { verifyToken, verifyAdmin } = require("../middlewares/auth");
 
+//User Routes
 router.post("/", reportController.createReport);
 
 router.get("/", reportController.getAllReports);
 
 router.get("/:id", reportController.getReportById);
 
-router.get("/:id", reportController.deleteReport);
+router.delete("/:id", reportController.deleteReport);
 
-router.get("/:id/status", reportController.updateStatus);
+// ADMIN ROUTES
+router.get("/admin/:id", verifyToken, verifyAdmin, reportController.getAdminReportDetail);
+
+router.put("/:id/status", verifyToken, verifyAdmin, reportController.updateReportStatus);
+
+router.delete("/admin/:id", verifyToken, verifyAdmin, reportController.deleteSpamReport);
+
 
 module.exports = router;
