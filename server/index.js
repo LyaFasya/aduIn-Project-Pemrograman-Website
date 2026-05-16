20c1f8fba9a657154893306c4b9c95909bf90231
ARIEL FARDIAN AYSEL <arielfardianaysel@gmail.com>
2026-05-16 13:57:30 +0700

    feat: Menambah fitur report

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 require('dotenv').config();
+
+const express = require("express");
+const app = express();
+
+const reportRoutes = require("./routes/report.route");
+
+app.use(express.json());
+
+app.use("/reports", reportRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
